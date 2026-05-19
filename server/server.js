b8f52eaece521aba9338c795117cc89de9271bc0
REMIXMEN <abbisyldrm1@gmail.com>
2026-05-19 08:10:14 +0300

    final debug socket

--- server/server.js
+++ server/server.js
@@ -1,116 +1,53 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
-
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("AURA SOCKET SERVER AKTIF");
+  res.send("AURA SOCKET SERVER AKTIF - FINAL");
 });
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+  cors: { origin: "*", methods: ["GET", "POST"] },
 });
 
-let waitingUser = null;
+let waiting = null;
 
 io.on("connection", (socket) => {
+  console.log("BAGLANDI:", socket.id);
 
-  console.log("KULLANICI BAGLANDI:", socket.id);
+  socket.emit("server-ready", socket.id);
 
   socket.on("find-match", () => {
+    console.log("MATCH ISTEDI:", socket.id);
 
-    console.log("MATCH ISTIYOR:", socket.id);
+    if (waiting && waiting !== socket.id) {
+      const partner = waiting;
+      waiting = null;
 
-    // kendisi zaten waiting ise
-    if (waitingUser && waitingUser.id === socket.id) {
-      return;
-    }
+      console.log("ESLESTI:", socket.id, partner);
 
-    // waiting user varsa eşleştir
-    if (waitingUser) {
-
-      const partner = waitingUser;
-
-      waitingUser = null;
-
-      const roomId = `room-${socket.id}-${partner.id}`;
-
-      socket.join(roomId);
-      partner.join(roomId);
-
-      socket.emit("matched", {
-        roomId,
-        partnerId: partner.id,
-      });
-
-      partner.emit("matched", {
-        roomId,
-        partnerId: socket.id,
-      });
-
-      console.log("ESLESTI:", roomId);
-
+      socket.emit("matched", { partnerId: partner });
+      io.to(partner).emit("matched", { partnerId: socket.id });
     } else {
-
-      waitingUser = socket;
-
-      socket.emit("waiting");
-
+      waiting = socket.id;
       console.log("BEKLIYOR:", socket.id);
-
+      socket.emit("waiting");
     }
-
   });
 
-  // WebRTC signal
-  socket.on("signal", ({ to, data }) => {
-
-    io.to(to).emit("signal", {
-      from: socket.id,
-      data,
-    });
-
-  });
-
-  // sonraki kullanıcı
-  socket.on("next", () => {
-
-    console.log("SONRAKI:", socket.id);
-
-    if (waitingUser && waitingUser.id === socket.id) {
-      waitingUser = null;
-    }
-
-    socket.emit("waiting");
-
-    waitingUser = socket;
-
-  });
-
-  // bağlantı çıkışı
   socket.on("disconnect", () => {
-
     console.log("CIKTI:", socket.id);
-
-    if (waitingUser && waitingUser.id === socket.id) {
-      waitingUser = null;
-    }
-
+    if (waiting === socket.id) waiting = null;
   });
-
 });
 
 const PORT = process.env.PORT || 3001;
-
 server.listen(PORT, () => {
   console.log("SERVER AKTIF:", PORT);
 });
